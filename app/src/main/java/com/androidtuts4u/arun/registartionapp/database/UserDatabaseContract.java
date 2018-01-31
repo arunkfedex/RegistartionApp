8a47b76bd10717c426f8996e7ed56c01a75b62a3
arunkfedex <arun05111990@gmail.com>
2018-01-31 22:12:47 +0530

    Update UserDatabaseContract.java
    column name change

--- app/src/main/java/com/androidtuts4u/arun/registartionapp/database/UserDatabaseContract.java
+++ app/src/main/java/com/androidtuts4u/arun/registartionapp/database/UserDatabaseContract.java
@@ -14,9 +14,9 @@ public final class UserDatabaseContract {
     public static class UserDatabase implements BaseColumns {
         public static final String TABLE_NAME = "user_details";
         public static final String COLUMN_NAME_COL1 = "name";
-        public static final String CLOMUMN_NAME_COL2 = "address";
+        public static final String COLUMN_NAME_COL2 = "address";
         public static final String COLUMN_NAME_COL3 = "phone_no";
-        public static final String COLUMN_NAME_COL_4 = "profession";
+        public static final String COLUMN_NAME_COL4 = "profession";
 
     }
 
